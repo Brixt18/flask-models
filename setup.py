f0aeb851611470231aa3ed242fee0a022e491ad3
Luca Mendez <lucamendez18@hotmail.com>
2022-10-14 19:42:41 -0300

    setup file

--- setup.py
+++ setup.py
@@ -1,20 +1,11 @@
-import setuptools
+from setuptools import find_packages, setup
 
-# with open("README.md", "r") as fh:
-#     long_description = fh.read()
-
-setuptools.setup(
-    name="flask-models",
-    version="0.0.1",
-    author="Brixt18",
-    description="Basics models for flask",
-    long_description="",
-    long_description_content_type="text/markdown",
-    url="https://github.com/Brixt18/flask-models",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+setup.setup(
+    name='flask_models',
+    packages=find_packages(include=["models"]),
+    version='0.0.2',
+    description='Basics models for Python-Flask',
+    author='Brixt18',
+    license='MIT',
+    setup_requires=['flask', "flask-login", "flask-sqlalchemy", ],
 )
