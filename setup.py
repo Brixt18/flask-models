b614fa2cafb045f7f907a73b09ded8d56f81eb2d
Luca Mendez <lucamendez18@hotmail.com>
2022-12-26 18:48:46 -0300

    rollback

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='flask_models',
     packages=find_packages(include=["flask_models"]),
-    version='0.1.4',
+    version='0.1.5',
     description='Basics models for Python-Flask',
     author='Brixt18',
     license='MIT',
--- src/flask_models/models.py
+++ src/flask_models/models.py
@@ -1,5 +1,4 @@
 import secrets
-from abc import abstractmethod
 from datetime import datetime
 from sqlite3 import IntegrityError
 
@@ -10,7 +9,7 @@ from .const import *
 
 
 class BaseQuery:
-	pass
+    pass
 
 
 def _generate_token(number=10):
@@ -18,129 +17,130 @@ def _generate_token(number=10):
 
 
 class _CRUD:
-	__abstract__ = True
+    __abstract__ = True
 
-	@abstractmethod
-	def __init__(self, check_auth: bool = True):
-		self.check_auth = check_auth
+    def _check_auth(self, check_auth):
+        if (check_auth) and (not current_user.is_authenticated):
+            return False
 
-	def _check_auth(self):
-		if (self.check_auth) and (not current_user.is_authenticated):
-			return False
+        return True
 
-		return True
+    def generate_token(self, length=15):
+        if not self.token:
+            self.token = _generate_token(15)
 
-	def generate_token(self, length=15):
-		if not self.token:
-			self.token = _generate_token(length)
+        while self._exist_token(self.token):
+            self.token = _generate_token(15)
+        
+    def save(self, check_auth: bool = True):
+        if self._check_auth(check_auth):
+            db.session.begin_nested()  # create checkpoint
+            db.session.add(self)
 
-		while self._exist_token(self.token):
-			self.token = _generate_token(length)
+            #  generate token
+            if not self.token:
+                self.token = _generate_token(15)
 
-	def save(self):
-		if self._check_auth():
-			db.session.begin_nested()  # create checkpoint
-			db.session.add(self)
+            while self._exist_token(self.token):
+                self.token = _generate_token(15)
 
-			self.generate_token()
+            # commit
+            try:
+                db.session.commit()
 
-			# commit
-			try:
-				db.session.commit()
+            except IntegrityError:
+                db.session.rollback()
 
-			except IntegrityError:
-				db.session.rollback()
+    def delete(self, check_auth: bool = True):
+        if self._check_auth(check_auth):
+            db.session.begin_nested()
+            try:
+                db.session.delete(self)
 
-	def delete(self):
-		if self._check_auth():
-			db.session.begin_nested()
-			try:
-				db.session.delete(self)
+                db.session.commit()
 
-				db.session.commit()
+            except IntegrityError:
+                db.session.rollback()
 
-			except IntegrityError:
-				db.session.rollback()
+    def update(self, data: dict, check_auth: bool = True):
+        if self._check_auth(check_auth):
+            db.session.begin_nested()
+            try:
+                for key, value in data.items():
+                    if hasattr(self, key):
+                        setattr(self, key, value)
 
-	def update(self, data: dict):
-		if self._check_auth():
-			db.session.begin_nested()
-			try:
-				for key, value in data.items():
-					if hasattr(self, key):
-						setattr(self, key, value)
+                if hasattr(self, "updated_at"):
+                    self.updated_at = datetime.utcnow()
 
-				if hasattr(self, "updated_at"):
-					self.updated_at = datetime.utcnow()
+                db.session.commit()
 
-				db.session.commit()
+            except IntegrityError as e:
+                db.session.rollback()
 
-			except IntegrityError as e:
-				db.session.rollback()
+    def _exist_token(self, token):
+        query = self.get_by_token(token)
 
-	def _exist_token(self, token):
-		query = self.get_by_token(token)
+        if (query is None) or (query.id == self.id):
+            return False
 
-		if (query is None) or (query.id == self.id):
-			return False
+        return True
 
-		return True
+    @classmethod
+    def bulk_save(cls, objects:list):
+        # if cls._check_auth(check_auth):
+        db.session.begin_nested()
 
-	@classmethod
-	def bulk_save(cls, objects: list):
-		if cls._check_auth():
-			db.session.begin_nested()
+        for obj in objects:
+            obj.generate_token()
 
-			for obj in objects:
-				obj.generate_token()
+        try:
+            db.session.add_all(objects)
+            db.session.commit()
 
-			try:
-				db.session.add_all(objects)
-				db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
 
-			except IntegrityError:
-				db.session.rollback()
+    @classmethod
+    def get_by_id(cls, id, or_404: bool = False) -> "object|None":
+        query = cls.query.filter_by(id=id).first()
 
-	@classmethod
-	def get_by_id(cls, id, or_404: bool = False) -> "object|None":
-		query = cls.query.filter_by(id=id).first()
+        if (not query) and (or_404):
+            abort(404)
 
-		if (not query) and (or_404):
-			abort(404)
+        return query
 
-		return query
+    @classmethod
+    def get_by_token(cls, token, or_404: bool = False) -> "object|None":
+        query = cls.query.filter_by(token=token).first()
 
-	@classmethod
-	def get_by_token(cls, token, or_404: bool = False) -> "object|None":
-		query = cls.query.filter_by(token=token).first()
+        if (not query) and (or_404):
+            abort(404)
 
-		if (not query) and (or_404):
-			abort(404)
+        return query
 
-		return query
+    @classmethod
+    def get_all(cls, limit: int = None, basequery: bool = False) -> "list|BaseQuery":
+        query = cls.query
 
-	@classmethod
-	def get_all(cls, limit: int = None, basequery: bool = False) -> "list|BaseQuery":
-		query = cls.query
+        query = query.limit(limit)
 
-		query = query.limit(limit)
-
-		return query if basequery else query.all()
+        return query if basequery else query.all()
 
 
 class Model(db.Model, _CRUD):
-	__abstract__ = True
+    __abstract__ = True
 
-	id = COLUMN(INTEGER, primary_key=True, autoincrement=True, nullable=False)
-	token = COLUMN(STRING(32), unique=True, nullable=False)
+    id = COLUMN(INTEGER, primary_key=True, autoincrement=True, nullable=False)
+    token = COLUMN(STRING(32), unique=True, nullable=False)
 
-	created_at = COLUMN(DATETIME, nullable=False, default=datetime.utcnow())
-	updated_at = COLUMN(DATETIME, nullable=False,
-                     default=datetime.utcnow(), onupdate=datetime.utcnow())
-	is_active = COLUMN(BOOLEAN, nullable=False, default=True)
+    created_at = COLUMN(DATETIME, nullable=False, default=datetime.utcnow())
+    updated_at = COLUMN(DATETIME, nullable=False,
+                        default=datetime.utcnow(), onupdate=datetime.utcnow())
+    is_active = COLUMN(BOOLEAN, nullable=False, default=True)
 
-	def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-	def __repr__(self):
-		return '<{}>'.format(self.__class__.__name__)
+    def __repr__(self):
+        return '<{}>'.format(self.__class__.__name__)
